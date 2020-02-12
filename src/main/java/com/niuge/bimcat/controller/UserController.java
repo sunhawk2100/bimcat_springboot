a0495918ead17259887f162f701689be0a02b5b3
254033230@qq.com <254033230@qq.com>
2020-02-12 17:59:18 +0800

    1、增加登陆验证码功能 2、增加logback日志配置

--- src/main/java/com/niuge/bimcat/controller/UserController.java
+++ src/main/java/com/niuge/bimcat/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.niuge.bimcat.controller;
 
+import com.niuge.bimcat.utils.ValidateCodeUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 @Controller
 public class UserController {
@@ -62,11 +72,48 @@ public class UserController {
         return "cloth";
     }
 
+    @RequestMapping("/logout")
+    public String logout() {
+        Subject subject = SecurityUtils.getSubject();
+        if (subject.isAuthenticated()) {
+            subject.logout(); // session 会销毁，在SessionListener监听session销毁，清理权限缓存
+            if (log.isDebugEnabled()) {
+                log.debug("用户退出登录");
+            }
+        }
+        return "/login";
+    }
+
+    /**
+     * 生成验证码
+     * @param request
+     * @param response
+     * @throws IOException
+     * @ValidateCode.generateTextCode(验证码字符类型,验证码长度,需排除的特殊字符)
+     * @ValidateCode.generateImageCode(文本验证码,图片宽度,图片高度,干扰线的条数,字符的高低位置是否随机,图片颜色,字体颜色,干扰线颜色)
+     */
+    @RequestMapping(value = "/validateCode")
+    public void validateCode(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setHeader("Cache-Control", "no-cache");
+        String verifyCode = ValidateCodeUtils.generateTextCode(ValidateCodeUtils.TYPE_NUM_LOWER, 4, null);
+        request.getSession().setAttribute("validateCode", verifyCode);
+        response.setContentType("image/jpeg");
+        BufferedImage bim = ValidateCodeUtils.generateImageCode(verifyCode, 90, 30, 5, true, Color.WHITE, Color.BLUE, null);
+        ImageIO.write(bim, "JPEG", response.getOutputStream());
+    }
+
     /**
      * 登录逻辑处理
      */
     @RequestMapping("/login")
-    public String login(String name,String password,Boolean isRememberMe,Model model){
+    public String login(String name,String password,String randomcode,Boolean isRememberMe,Model model){
+
+        Session session = SecurityUtils.getSubject().getSession();
+        String code = (String) session.getAttribute("validateCode");
+        if (StringUtils.isEmpty(randomcode) || !code.equals(randomcode.toLowerCase())) {
+            model.addAttribute("msg", "验证码错误");
+            return "login";
+        }
 
         log.info("name="+name);
         /**
--- src/main/java/com/niuge/bimcat/shiro/ShiroConfig.java
+++ src/main/java/com/niuge/bimcat/shiro/ShiroConfig.java
@@ -50,6 +50,7 @@ public class ShiroConfig {
         filterMap.put("/index", "user");
         //放行login.html页面
         filterMap.put("/login", "anon");
+        filterMap.put("/validateCode", "anon");
 
         //授权过滤器
         //注意：当前授权拦截后，shiro会自动跳转到未授权页面
